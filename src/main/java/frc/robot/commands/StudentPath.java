b9ade96c264d7d4deea01d3be28d4c9f3705fca4
Colin <hungrymamamia@gmail.com>
2023-08-22 22:49:10 -0700

    A

---
+++ src/main/java/frc/robot/commands/StudentPath.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class StudentPath extends SequentialCommandGroup {
+  /**
+   * Create your own path! Refer to the powerpoint for directions
+   *
+   * @param drivetrain The drivetrain subsystem on which this command will run
+   */
+  public StudentPath(Drivetrain drivetrain) {
+    addCommands(
+        new DriveDistance(-0.5, 10, drivetrain),
+        new TurnDegrees(-0.5, 180, drivetrain),
+        new DriveDistance(-0.5, 10, drivetrain),
+        new TurnDegrees(0.5, 180, drivetrain));
+  }
+}
